30ef7a39da784af32543d9014e94ae38d52fb367
Nyx <nv.schutz@gmail.com>
2024-05-14 16:33:53 +0200

    trying to make visual navbar work

--- script/init.js
+++ script/init.js
@@ -1,3 +1,10 @@
 function onLoad() {
     includeHTML();
+    const links = document.querySelectorAll('.navlink');
+    const thisLink = location.href.split("/").slice(-1).toString().replace(".html", "").split("?")[0];
+  console.log (links, thisLink)
+  links.forEach(link => {
+    link.classList.remove('activepage');
+  });
+  currentID.classList.add("activepage");
 }
