24cc443077991c8b54c12da787d2a467d542798c
Nyx <nv.schutz@gmail.com>
2024-05-15 13:03:04 +0200

    get the user's language pref

--- script/lang.js
+++ script/lang.js
@@ -1,12 +1,17 @@
-var language; 
-function getLanguage() {
-(localStorage.getItem('language') == null) ? setLanguage('fr') : false;
-$.ajax({ 
-url:  '/language/' +  localStorage.getItem('language') + '.json', 
-dataType: 'json', async: false, dataType: 'json', 
-success: function (lang) { language = lang } });
+function setLang() {
+    const userLang = navigator.language.slice(0, 2);
+    console.log(userLang);
 }
 
-function setLanguage(lang) {
-localStorage.setItem('language', lang);
-}
+// var language; 
+// function getLanguage() {
+// (localStorage.getItem('language') == null) ? setLanguage('fr') : false;
+// $.ajax({ 
+// url:  '/language/' +  localStorage.getItem('language') + '.json', 
+// dataType: 'json', async: false, dataType: 'json', 
+// success: function (lang) { language = lang } });
+// }
+
+// function setLanguage(lang) {
+// localStorage.setItem('language', lang);
+// }
