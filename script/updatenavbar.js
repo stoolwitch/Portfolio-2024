675874c0c4b33094fcd4b8e22c3d473875190123
Nyx <nv.schutz@gmail.com>
2024-05-27 18:09:33 +0200

    first time load bug fixed

--- script/updatenavbar.js
+++ script/updatenavbar.js
@@ -1,11 +1,15 @@
 function navBar() {
     const links = document.querySelectorAll('.navlink');
+    const indexLink = document.getElementById('index');
     const thisLink = location.href.split("/").slice(-1).toString().replace(".html", "").split("?")[0];
     const currentID = document.getElementById(thisLink);
     //console.log(links, thisLink)
     links.forEach(link => {
         link.classList.remove('activepage');
     });
+    if (thisLink == ""){
+        indexLink.classList.add("activepage");
+    };
     currentID.classList.add("activepage");
     var activepagename = document.getElementById("activepagename");
     const currentPage = document.title;
