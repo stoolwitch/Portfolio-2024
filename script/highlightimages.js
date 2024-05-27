b113b41afbe9e276085085a041fd33005fa75aa0
Nyx <nv.schutz@gmail.com>
2024-05-27 12:21:04 +0200

    bug fixing js + links in playground

--- script/highlightimages.js
+++ script/highlightimages.js
@@ -1,9 +1,13 @@
+// var x = 1;
+
 function highlightsize(){
     const highImages = document.querySelectorAll('.project-gallery');
     highImages.forEach(image => {
         image.style.height = "fit-content";
         var imageheight = image.clientHeight;
         var halfheight = imageheight / 1.96;
+        // var txt = x += 1;
+        // console.log("txt=" + txt);
         image.style.height = halfheight + "px";
     });
 }
